31eb66f24903616e88b6bad4b82b12f7c87bfd12
ATOMOSX <112136920+ATOMOSX@users.noreply.github.com>
2022-11-26 11:03:49 -0500

    Se agregan dos clases nuevas
    Se agregan dos clases nuevas de la seccion 5 para el estudio.
    
    se analiza el while, do while, for each, hasta el momento metodos muy basicos

---
+++ src/main/java/seccion5/SentenciaForEach.java
@@ -0,0 +1,16 @@
+package seccion5;
+
+public class SentenciaForEach {
+    public static void main(String[] args) {
+
+        int[] numerosArreglo = {1 , 3, 5, 7, 9, 11, 13, 15};
+        for (int num : numerosArreglo){
+            System.out.println("num = " + num);
+        }
+        
+        String[] nombres = {"andres", "maria", "paco", "juan", "dani", "eileen", "diana", "haydee"};
+        for (String nom : nombres){
+            System.out.println("nom = " + nom);
+        }
+    }
+}
---
+++ src/main/java/seccion5/SentenciaWhile.java
@@ -0,0 +1,42 @@
+package seccion5;
+
+public class SentenciaWhile {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 5){
+            System.out.println("i = " + i);
+            i++;
+        }
+
+        i = 0;
+        boolean prueba = true;
+        while (prueba){
+            if (i == 7){
+                prueba = false;
+            }
+            System.out.println(i);
+            i++;
+        }
+
+        prueba = false;
+        while (prueba){
+            System.out.println("Nunca se ejecuta porque es falso");
+        }
+
+        do {
+            System.out.println("Esto se ejecuta al menos una sola vez");
+        }while (prueba);
+
+        prueba = true;
+        i = 0;
+        do {
+            if (i == 10){
+                prueba = false;
+            }
+            System.out.println("i = " + i);
+            i++;
+        }while (prueba);
+    }
+}
