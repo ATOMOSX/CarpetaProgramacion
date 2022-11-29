a74740efd69a3a2849e24e7e8950aa5eb3b0bb49
ATOMOSX <juand.lopezm@uqvirtual.edu.co>
2022-11-29 18:32:12 -0500

    se agrega una clase que faltaba

---
+++ src/main/java/seccion5/SentenciasDeBucleBuscar.java
@@ -0,0 +1,27 @@
+package seccion5;
+
+public class SentenciasDeBucleBuscar {
+    public static void main(String[] args) {
+
+        String frase = "Tres tristes tigres tragan trigo en un trigal";
+        String palabra = "trigo";
+
+        int maxPalabra = palabra.length();
+        int maxFrase = frase.length();
+
+
+        int cantidad = 0;
+        buscar:
+        for (int i = 0; i < maxFrase; i++){
+            int k = i;
+            for (int j = 0; j < maxPalabra; j++) {
+                if (frase.toUpperCase().charAt(k++) != palabra.toUpperCase().charAt(j)) {
+                    continue buscar;
+                }
+            }
+            cantidad++;
+        }
+        System.out.println("Encontrado un total de = " + cantidad + " la palabra " + palabra + " en la frase: "
+                            + frase.toUpperCase());
+    }
+}
