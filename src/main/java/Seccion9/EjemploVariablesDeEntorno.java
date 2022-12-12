4e9eedf0cd13664d308909b899100ebffb37dfa2
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2022-12-12 16:44:19 -0500

    cambios pequeños

--- src/main/java/Seccion9/EjemploVariablesDeEntorno.java
+++ src/main/java/Seccion9/EjemploVariablesDeEntorno.java
@@ -1,7 +1,17 @@
 package Seccion9;
 
+import java.util.Map;
+
 public class EjemploVariablesDeEntorno {
     public static void main(String[] args) {
-        
+
+        Map<String, String> varEnv = System.getenv();
+        System.out.println("variables ambientes del sistema = " + varEnv);
+
+        String username = System.getenv("USERNAME");
+        System.out.println("username = " + username);
+
+        String javaHome = System.getenv("JAVA_HOME");
+        System.out.println("javaHome = " + javaHome);
     }
 }
