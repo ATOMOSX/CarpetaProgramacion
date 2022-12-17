c67abd4df24a154af6a334f86c52c37fa07d5a4b
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2022-12-17 16:06:40 -0500

    clase desplazar arreglos una posicion

--- src/main/java/Seccion12/EjemploArreglosDesplazadosPorPosicion2.java
+++ src/main/java/Seccion12/EjemploArreglosDesplazadosPorPosicion2.java
@@ -2,25 +2,30 @@ package Seccion12;
 
 import java.util.Scanner;
 
-public class EjemploArreglosDesplazadosPorPosicion {
+public class EjemploArreglosDesplazadosPorPosicion2 {
     public static void main(String[] args) {
 
         int[] numero = new int[10];
-        int ultimo;
+        int elemento, posicion;
 
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < numero.length; i++){
+        for (int i = 0; i < numero.length - 1; i++){
             System.out.print("ingrese el numero: ");
             numero[i] = scanner.nextInt();
         }
         System.out.println();
 
-        ultimo = numero[numero.length - 1];
-        for (int i = numero.length - 2; i >= numero.length; i--){
+        System.out.println("Nuevo elemento: ");
+        elemento = scanner.nextInt();
+
+        System.out.println("Posicion donde agregar el elemento de 0 a 9");
+        posicion = scanner.nextInt();
+
+        for (int i = numero.length - 2; i >= posicion; i--){
             numero[i + 1] = numero[i];
         }
-        numero[0] = ultimo;
+        numero[posicion] = elemento;
 
         System.out.println("El arreglo: ");
         for (int i = 0; i < numero.length; i++){
