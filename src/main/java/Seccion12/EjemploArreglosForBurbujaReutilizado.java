de747688d0a4cf06e28ba5d528dcf645ad8408d4
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2022-12-16 20:19:09 -0500

    .

--- src/main/java/Seccion12/EjemploArreglosForBurbujaReutilizado.java
+++ src/main/java/Seccion12/EjemploArreglosForBurbujaReutilizado.java
@@ -13,27 +13,47 @@ public class EjemploArreglosForBurbujaReutilizado {
             total2--;
         }
     }
+
+    public static void sortBurbuja(Object[] arreglo){
+
+        int total = arreglo.length;
+        int contador = 0;
+
+        for (int i = 0; i < total ;i++){
+            for (int j = 0; j < total - 1 - i; j++){
+                if ( ((Comparable) arreglo[j + 1]).compareTo(arreglo[j]) < 0){
+                    Object aux = arreglo[j];
+                    arreglo[i] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                }
+                contador++;
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         String[] productos = {"TV", "Computador todo en uno", "Iphone", "SSD", "Disco duro", "Motorola", "Moto"};
         int total = productos.length;
 
-        int contador = 0;
-        for (int i = 0; i < total;i++){
-            for (int j = 0; j < total - 1 - i; j++){
-                if (productos[j + 1].compareTo(productos[j]) < 0){
-                    String aux = productos[j];
-                    productos[i] = productos[j + 1];
-                    productos[j + 1] = aux;
-                }
-                contador++;
-            }
-        }
-        System.out.println("contador = " + contador);
+        sortBurbuja(productos);
 
         System.out.println("===== Usando for =====");
         for (int i = 0; i < total; i++) {
             System.out.println("Para el indice  " + i + ": " + productos[i]);
+        }
+
+        Integer [] numero = new Integer[4];
+
+        numero[0] = 1;
+        numero[1] = 2;
+        numero[2] = 3;
+        numero[3] = 4;
+
+        sortBurbuja(numero);
+
+        for (int i = 0; i < numero.length; i++){
+            System.out.println("Numero " + i + " : " + numero[i ]);
         }
     }
 }
