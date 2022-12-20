09ea83a2ec52845be9e86243e968f5d2cd466a89
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2022-12-19 20:34:10 -0500

    clase eliminar elemento de un arreglo

---
+++ src/main/java/Seccion12/EjemplosArreglosEliminarElemento.java
@@ -0,0 +1,29 @@
+package Seccion12;
+
+import java.util.Scanner;
+
+public class EjemplosArreglosEliminarElemento {
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < a.length; i++){
+            System.out.println("Ingrese un numero: ");
+            a[i] = scanner.nextInt();
+        }
+        System.out.println();
+
+        System.out.println("Ingrese una posicion a eliminar entre 0-9");
+        int posicion = scanner.nextInt();
+
+        for (int i = posicion; i < a.length - 1; i++){
+            a[i] = a[i + 1];
+        }
+
+        for (int i = 0; i < a.length - 1; i++){
+            System.out.println(i + " => " + a[i]);
+        }
+    }
+}
