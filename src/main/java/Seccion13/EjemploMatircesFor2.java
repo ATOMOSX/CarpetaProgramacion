85386d51f2215d42a210e503a9cb31b460bf1889
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2022-12-22 10:06:22 -0500

    ejemplo numero 2matrices for

---
+++ src/main/java/Seccion13/EjemploMatircesFor2.java
@@ -0,0 +1,17 @@
+package Seccion13;
+
+public class EjemploMatircesFor2 {
+    public static void main(String[] args) {
+
+        String[][] nombres = {{"Pepe", "Pepa"}, {"Josefa", "Hugo"}, {"Paco", "pepe"}};
+        
+        for (String[] fila : nombres){
+            for (String nombre : fila){
+                System.out.print(nombre + " \t " );
+            }
+            System.out.println();
+        }
+
+
+    }
+}
