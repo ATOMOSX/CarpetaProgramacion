2ecb9bca9066bce4bb20fc38f6f5cce0364b6d10
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2022-12-27 16:17:36 -0500

    utilizando el enum con un swich mejorado

---
+++ Poo/src/main/java/EjemploAutomovilEnum.java
@@ -0,0 +1,39 @@
+public class EjemploAutomovilEnum {
+    public static void main(String[] args) {
+
+        Automovil subaru = new Automovil("Subaru", "Impreza" );
+        subaru.setCilindrada(1926.5);
+        subaru.setColor(Color.AZUL);
+        subaru.setTipo(TipoAtumovil.CUPE);
+        System.out.println(subaru.detalle());
+
+        Automovil mazda = new Automovil("Mazda", "Mx-5");
+        mazda.setCilindrada(3000.0);
+        mazda.setColor(Color.ROJO);
+        mazda.setTipo(TipoAtumovil.HATCHBACK);
+        System.out.println(mazda.detalle());
+
+        TipoAtumovil tipo = subaru.getTipo();
+        System.out.println("Tipo subaru = " + tipo.getNombre());
+        System.out.println("Tipo subaru descripcion = " + tipo.getDescripcion());
+
+        tipo = mazda.getTipo();
+
+        switch (tipo) {
+            case CONVERTIBLE -> System.out.println("El automovil es deportivo y descapotable de dos puertas");
+            case CUPE -> System.out.println("El automovil pequeño de dos puertas y tipicamente deportivo");
+            case FURGON -> System.out.println("Es un automovil utilitario de transporte, de empresas");
+            case HATCHBACK -> System.out.println("Es un automovil mediano, aspecto deportivo");
+            case SEDAN -> System.out.println("Es un automovil mediano o commune y corriente");
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
