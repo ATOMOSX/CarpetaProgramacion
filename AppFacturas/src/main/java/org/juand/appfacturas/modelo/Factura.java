358e4a21ad7f9e5d11e0c0d3610c29732ae88ccd
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-01-04 16:33:58 -0500

    creamos las clases del modelo factura

---
+++ AppFacturas/src/main/java/org/juand/appfacturas/modelo/Factura.java
@@ -0,0 +1,67 @@
+package org.juand.appfacturas.modelo;
+
+import java.util.Date;
+
+public class Factura {
+
+    private int folio;
+    private String descripcion;
+    private Date fecha;
+    private Cliente cliente;
+    private ItemFactura[] items;
+    private int indiceItems;
+
+    public static final int MAX_ITEMS = 12;
+    private static int ultimoFolio;
+
+    public Factura(String descripcion, Cliente cliente) {
+        this.descripcion = descripcion;
+        this.cliente = cliente;
+        this.items = new ItemFactura[MAX_ITEMS];
+        this.folio = ++ultimoFolio;
+        this.fecha = new Date();
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ItemFactura[] getItems() {
+        return items;
+    }
+
+    public void addItemFactura(ItemFactura itemFactura){
+        if (indiceItems < MAX_ITEMS){
+            this.items[indiceItems] = itemFactura;
+        }
+
+    }
+}
---
+++ AppFacturas/src/main/java/org/juand/appfacturas/modelo/ItemFactura.java
@@ -0,0 +1,28 @@
+package org.juand.appfacturas.modelo;
+
+public class ItemFactura {
+
+    private int cantidad;
+    private Producto producto;
+
+    public ItemFactura(int cantidad, Producto producto) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
