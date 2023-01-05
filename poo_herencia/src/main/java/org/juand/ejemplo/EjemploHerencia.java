906bcb59b4182d8ac7711952d78dd317215d4f35
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-01-05 18:33:45 -0500

    se cambian de carpetas
    se modifica la carpeta para probar mediante protected y ver que cuando estan en diferentes carpetas no se pueden acceder de forma normal, solo se puede acceder con sus getters y setters

--- poo_herencia/src/main/java/org/juand/ejemplo/EjemploHerencia.java
+++ poo_herencia/src/main/java/org/juand/ejemplo/EjemploHerencia.java
@@ -1,4 +1,6 @@
-package org.juand.pooherencia;
+package org.juand.ejemplo;
+
+import org.juand.pooherencia.*;
 
 public class EjemploHerencia {
     public static void main(String[] args) {
@@ -6,13 +8,14 @@ public class EjemploHerencia {
         Alumno alumno = new Alumno();
         alumno.setNombre("Juan David");
         alumno.setApellido("Lopez Muñoz");
-
+        alumno.setInstitucion("I.E Marcelino Champagnat");
         Profesor profesor = new Profesor();
         profesor.setNombre("Daniela");
         profesor.setApellido("Gomez");
         profesor.setAsignatura("Matematicas");
 
-        System.out.println("Alumno: " + alumno.getNombre() + " " + alumno.getApellido() + " ");
+        System.out.println("Alumno: " + alumno.getNombre() + " " + alumno.getApellido() +
+                " " + alumno.getInstitucion());
         System.out.println("Profesor de " + profesor.getAsignatura() + ": " + profesor.getNombre() +
                 " " + profesor.getApellido() + " " );
     }
