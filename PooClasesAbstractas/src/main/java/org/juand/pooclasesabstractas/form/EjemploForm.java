60e875ddc374503db2cdaccfdebc41133f005d74
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-01-10 17:09:01 -0500

    dibujamos los elementos y creamos un file html

--- PooClasesAbstractas/src/main/java/org/juand/pooclasesabstractas/form/EjemploForm.java
+++ PooClasesAbstractas/src/main/java/org/juand/pooclasesabstractas/form/EjemploForm.java
@@ -1,8 +1,11 @@
 package org.juand.pooclasesabstractas.form;
 
-import org.juand.pooclasesabstractas.form.elementos.ElementoForm;
 import org.juand.pooclasesabstractas.form.elementos.*;
 import org.juand.pooclasesabstractas.form.elementos.select.Opcion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class EjemploForm {
     public static void main(String[] args) {
@@ -25,17 +28,32 @@ public class EjemploForm {
 
         SelectForm lenguahe = new SelectForm("lenguaje");
         Opcion java = new Opcion("1", "Java");
-        lenguahe.addOpcion(java);
-        lenguahe.addOpcion(new Opcion("2", "Python"));
-        lenguahe.addOpcion(new Opcion("3", "JavaScript"));
-        lenguahe.addOpcion(new Opcion("4", "TypeScript"));
-        lenguahe.addOpcion(new Opcion("5", "PHP"));
+        lenguahe.addOpcion(java)
+                .addOpcion(new Opcion("2", "Python"))
+                .addOpcion(new Opcion("3", "JavaScript"))
+                .addOpcion(new Opcion("4", "TypeScript"))
+                .addOpcion(new Opcion("5", "PHP"));
 
         username.setValor("juand.lopezm");
         pasword.setValor("12345");
         email.setValor("juand.lopezm@correo.edu.co");
         edad.setValor("22");
+        experiencia.setValor("... Mas de 2 años de experiencia ...");
         java.setSelected(true);
 
+        List<ElementoForm> elementos = Arrays.asList(username, pasword, email,
+                email, experiencia, lenguahe);
+
+        //Esta es una forma de iterar
+        for (ElementoForm elementoForm : elementos){
+            System.out.println(elementoForm.dibujarHtml());
+            System.out.println("<br>");
+        }
+
+        //Esta es otra forma de iterar mediante un forEach
+        elementos.forEach(el ->{
+            System.out.println(el.dibujarHtml());
+            System.out.println("<br>");
+        });
     }
 }
