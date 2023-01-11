9afdbc193d53d45cae945dbcad5dc6f4c1c996ba
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-01-11 09:11:01 -0500

    creamos la clase validador email

---
+++ PooClasesAbstractas/src/main/java/org/juand/pooclasesabstractas/form/validador/EmailValidador.java
@@ -0,0 +1,23 @@
+package org.juand.pooclasesabstractas.form.validador;
+
+public class EmailValidador extends Validador{
+
+    protected String mensaje = "El formato del email es invalido";
+
+    private final static String EMAIL_REGEX = "^(.+)@(.+)$";
+
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        return valor.matches(EMAIL_REGEX);
+    }
+}
