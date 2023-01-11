57dd0237b8c0d31f9eff3818314eb5e39ffb5191
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-01-11 09:34:30 -0500

    se agregan metodos es elemento form
    se agregan los metodos de add validador y si es valido

--- PooClasesAbstractas/src/main/java/org/juand/pooclasesabstractas/form/elementos/ElementoForm.java
+++ PooClasesAbstractas/src/main/java/org/juand/pooclasesabstractas/form/elementos/ElementoForm.java
@@ -1,11 +1,21 @@
 package org.juand.pooclasesabstractas.form.elementos;
 
+import org.juand.pooclasesabstractas.form.validador.Validador;
+
+import java.util.ArrayList;
+import java.util.List;
+
 abstract public class ElementoForm {
 
     protected String valor;
     protected String nombre;
 
+    private List<Validador> validadors;
+    private List<String> errores;
+
     public ElementoForm() {
+        this.validadors = new ArrayList<>();
+        this.errores = new ArrayList<>();
     }
 
     public ElementoForm(String nombre) {
@@ -13,8 +23,26 @@ abstract public class ElementoForm {
         this.nombre = nombre;
     }
 
+    public ElementoForm addValidador(Validador validador){
+        this.validadors.add(validador);
+        return this;
+    }
+
+    public boolean esValido(){
+        for (Validador validador : this.validadors){
+            if (!validador.esValido(this.valor)){
+                this.errores.add(validador.getMensaje());
+            }
+        }
+        return this.errores.isEmpty();
+    }
+
     public void setValor(String valor) {
         this.valor = valor;
+    }
+
+    public List<String> getErrores() {
+        return errores;
     }
 
     abstract public String dibujarHtml();
