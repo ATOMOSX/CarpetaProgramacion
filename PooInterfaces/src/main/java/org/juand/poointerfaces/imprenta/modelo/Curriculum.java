8a205fabf99ac85f11eb893f816593d5807e67f0
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-01-13 06:39:31 -0500

    creamos una clase concreta a la jerarquia

---
+++ PooInterfaces/src/main/java/org/juand/poointerfaces/imprenta/modelo/Curriculum.java
@@ -0,0 +1,36 @@
+package org.juand.poointerfaces.imprenta.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curriculum extends Hoja{
+
+    private String persona;
+    private String carrera;
+    private List<String> experiencias;
+
+    public Curriculum(String contenido, String persona, String carrera) {
+        super(contenido);
+        this.persona = persona;
+        this.carrera = carrera;
+        this.experiencias = new ArrayList<>();
+    }
+
+    public Curriculum addExperiencia(String experiencia){
+        this.experiencias.add(experiencia);
+        return this;
+    }
+
+    @Override
+    public String imprimir() {
+        StringBuilder stringBuilder = new StringBuilder("Nombre: ");
+        stringBuilder.append(persona).append("\n")
+                .append("Resumen: ").append(this.contenido)
+                .append("\n").append("Profesion: ").append(carrera)
+                .append("\n").append("Experiencias: ").append("\n");
+        for (String experiencias : this.experiencias){
+            stringBuilder.append("- ").append(experiencias).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
