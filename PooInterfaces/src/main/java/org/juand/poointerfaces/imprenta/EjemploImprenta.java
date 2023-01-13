846e1968733cfa1e2f48a8b3689c3f47a0da97e3
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-01-13 06:46:59 -0500

    Creamos la clase de ejemplo con el metodo main

--- PooInterfaces/src/main/java/org/juand/poointerfaces/imprenta/EjemploImprenta.java
+++ PooInterfaces/src/main/java/org/juand/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,7 +1,26 @@
 package org.juand.poointerfaces.imprenta;
 
+import org.juand.poointerfaces.imprenta.modelo.Curriculum;
+import org.juand.poointerfaces.imprenta.modelo.Hoja;
+import org.juand.poointerfaces.imprenta.modelo.Informe;
+
 public class EjemploImprenta {
     public static void main(String[] args) {
 
+        Curriculum curriculum = new Curriculum("Resumen laboral", "Juan David", "Ingenieria de sistemas y computation");
+        curriculum.addExperiencia("Java");
+        curriculum.addExperiencia("PHP");
+        curriculum.addExperiencia("Sql");
+        curriculum.addExperiencia("Html5");
+        curriculum.addExperiencia("Css");
+
+        Informe informe = new Informe("Estudio sobre micro servicios", "Daniela Gomez", "Sasha");
+
+        imprimir(curriculum);
+        imprimir(informe);
+    }
+
+    public static void imprimir(Hoja imprimible){
+        System.out.println(imprimible.imprimir());
     }
 }
