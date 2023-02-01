91879c43a23e5ca353831cebe63562c4093e1d41
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-02-01 06:24:25 -0500

    Metodo join vs metodo sleep

--- Hilos/src/org/juand/hilos/InterfaceRunnableJava8.java
+++ Hilos/src/org/juand/hilos/InterfaceRunnableJava8.java
@@ -3,8 +3,9 @@ package org.juand.hilos;
 import org.juand.hilos.runnable.ViajeTarea;
 
 public class InterfaceRunnableJava8 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Thread main = Thread.currentThread();
         Runnable viaje  = () -> {
 
                 for (int i = 0; i < 10; i++){
@@ -16,15 +17,29 @@ public class InterfaceRunnableJava8 {
                     }
                 }
                 System.out.println("Finalmente me voy de viaje a: " + Thread.currentThread().getName());
-
+                System.out.println(main.getState());
             };
 
 
-        new Thread(viaje, "Isla de pascua").start();
-        new Thread(viaje, "Islas perdidas").start();
-        new Thread(viaje, "Juan Fernandez").start();
-        new Thread(viaje, "Miami").start();
-        new Thread(viaje, "Cancún").start();
+        Thread v1 = new Thread(viaje, "Isla de pascua");
+        Thread v2 = new Thread(viaje, "Islas perdidas");
+        Thread v3 = new Thread(viaje, "Juan Fernandez");
+        Thread v4 = new Thread(viaje, "Miami");
+        Thread v5 = new Thread(viaje, "Cancún");
+
+        v1.start();
+        v2.start();
+        v3.start();
+        v4.start();
+        v5.start();
+        v1.join();
+        v2.join();
+        v3.join();
+        v4.join();
+        v5.join();
+
+       //Thread.sleep(8000);
+        System.out.println("Continuando con la ejecucion del metodo main: " + main.getName());
 
     }
 }
